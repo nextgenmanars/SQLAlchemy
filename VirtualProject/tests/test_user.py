876d6e35350144cffa0615774990bd48ee15cbd1
nextgenmanars <146085141+nextgenmanars@users.noreply.github.com>
2024-03-05 12:08:39 +0300

    Update test_user.py

--- VirtualProject/tests/test_user.py
+++ VirtualProject/tests/test_user.py
@@ -4,13 +4,19 @@ from user import User
 
 
 @pytest.fixture(scope="module")
-def users():
-    users = session.query(User).all()
-    yield users
+def db_session():
+    create_session = session
+    yield create_session
     session.rollback()
     session.close()
 
 
-def test_query_user(users):
-    for user in users:
-        assert user.first_name.isalpha()
+@pytest.fixture(scope="module")
+def user(db_session):
+    user_id = 1
+    user = db_session.query(User).get(user_id)
+    return user
+
+
+def test_query_user(user):
+    assert user.first_name == "Dmitriy"
