3a0b82d905a8b46ca54a58a93ecb80bee03deada
nextgenmanars <146085141+nextgenmanars@users.noreply.github.com>
2024-03-15 11:48:06 +0300

    Delete VirtualProject directory

--- VirtualProject/models/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-import sqlalchemy
-
-engine = create_engine("postgresql+psycopg2://postgres:12580@localhost/task2_sqlalchemy")
-Base = sqlalchemy.orm.declarative_base()
-session = Session(bind=engine)
