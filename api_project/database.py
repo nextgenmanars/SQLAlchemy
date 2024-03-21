81a2b0b2a88897a0dff052986522ed1ceaa7f42e
Arseniy Samoylenko <146085141+nextgenmanars@users.noreply.github.com>
2024-03-21 17:46:09 +0300

    Delete api_project directory

--- api_project/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-import sqlalchemy
-
-engine = create_engine("postgresql+psycopg2://nextgenman:12345678@database:5432/postgres_db")
-Base = sqlalchemy.orm.declarative_base()
-session = Session(bind=engine)
